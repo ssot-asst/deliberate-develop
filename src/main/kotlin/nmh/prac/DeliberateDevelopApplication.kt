2262d74183bc656eecd9162a313820bfa878a8db
NaMinhyeok <nmh9097@gmail.com>
2025-06-18 21:23:23 +0900

    Initial commit

---
+++ src/main/kotlin/nmh/prac/DeliberateDevelopApplication.kt
@@ -0,0 +1,11 @@
+package nmh.prac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeliberateDevelopApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeliberateDevelopApplication>(*args)
+}
---
+++ src/test/kotlin/nmh/prac/DeliberateDevelopApplicationTests.kt
@@ -0,0 +1,13 @@
+package nmh.prac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeliberateDevelopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
