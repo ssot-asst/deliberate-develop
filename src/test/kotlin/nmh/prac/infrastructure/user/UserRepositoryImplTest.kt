21846925e689b4ec6a84afaec4c5e228e12801a3
NaMinhyeok <nmh9097@gmail.com>
2025-06-26 22:32:21 +0900

    feat: UserRepositoryImpl에 대한 동시성 및 OOM 테스트 추가 [#2]

---
+++ src/test/kotlin/nmh/prac/infrastructure/user/UserRepositoryImplTest.kt
@@ -0,0 +1,67 @@
+package nmh.prac.infrastructure.user
+
+import org.assertj.core.api.BDDAssertions.then
+import org.assertj.core.api.BDDAssertions.thenThrownBy
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Description
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+
+@SpringBootTest
+class UserRepositoryImplTest {
+
+    @Autowired
+    private lateinit var userRepository: UserRepositoryImpl
+
+    @Autowired
+    private lateinit var userJpaRepository: UserJpaRepository
+
+    @AfterEach
+    fun tearDown() {
+        userJpaRepository.deleteAllInBatch()
+    }
+
+    @Test
+    fun `사용자 요청이 동시에 일어날 경우를 확인한다`() {
+        // given
+        val threadCount = 20
+        val executorService = Executors.newFixedThreadPool(10)
+        val latch = CountDownLatch(threadCount)
+
+        // when
+        for (i in 0 until threadCount) {
+            executorService.execute {
+                try {
+                    userRepository.save(UserJpaEntity(name = "나민혁$i", age = 26 + i))
+                } finally {
+                    latch.countDown()
+                }
+            }
+        }
+
+        latch.await()
+        // then
+        then(userJpaRepository.findAll()).hasSize(20)
+    }
+
+    @Description("해당 테스트는 무시가 된다. 이유를 알 수 없음")
+    @Test
+    fun `한번에 조회하는 데이터가 너무 많으면 OOM이 일어난다`() {
+        // given
+        val largeDataCount = 1_000_000_000
+        val users = (1..largeDataCount).map { 
+            UserJpaEntity(name = "User$it", age = it % 100 + 1)
+        }
+        userJpaRepository.saveAll(users)
+
+        // when
+        // then
+        thenThrownBy {
+            userRepository.findAll()
+        }.isInstanceOf(OutOfMemoryError::class.java)
+    }
+}
